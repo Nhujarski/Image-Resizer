825aeacf16334b8a74aea457afad66c90dbcd9eb
Nick Hujarski <nickhujarski@Roberts-13-inch-MacBook-Pro.local>
2023-03-15 12:21:27 -0700

    initial set up of project

---
+++ main.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const { app, BrowserWindow, Menu } = require('electron');
+
+const isDev = process.env.NODE_ENV != 'development';
+const isMac = process.platform === 'darwin';
+
+// Create Main windiow
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'Image Resizer',
+    width: isDev ? 1000 : 500,
+    height: 600,
+  });
+
+  // open dev tools if in dev enviroment
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+// Create About Window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: 'About Image Resizer',
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
+// App is ready
+app.whenReady().then(() => {
+  createMainWindow();
+
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+// Menu Template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
